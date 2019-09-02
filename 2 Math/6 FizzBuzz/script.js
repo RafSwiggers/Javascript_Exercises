09e99b05ecf11d30a3ae6197827cfe8626f03424
RafSwiggers <rafswiggers@gmail.com>
2019-09-02 12:30:35 +0200

    add solution for fizzbuzz

--- 2 Math/6 FizzBuzz/script.js
+++ 2 Math/6 FizzBuzz/script.js
@@ -10,7 +10,17 @@
 // You will have time to focus on it later.
 
 (function() {
-
+for(var i =1; i<100; i++){
+    if (i%3==0 && i%5 == 0){
+        console.log("Fizzbuzz")
+    } else if(i%3 == 0){
+        console.log("fizz");
+    } else if (i%5 == 0) {
+        console.log("buzz");
+    } else {
+        console.log(i);
+    }
+};
     // your code here
 
 })();
