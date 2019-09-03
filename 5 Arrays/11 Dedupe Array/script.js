9ade8d01d3e082ff9b8b10963a4f6e5c5d197b39
RafSwiggers <rafswiggers@gmail.com>
2019-09-03 11:50:41 +0200

    solve 5.11

---
+++ 5 Arrays/11 Dedupe Array/script.js
@@ -0,0 +1,42 @@
+/* becode/javascript
+ *
+ * /05-arrays/11-dedupe-array/script.js - 5.11: dédoublonnement d'un tableau
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+    var fruits = [
+        "cerise",
+        "durian",
+        "pomme",
+        "poire",
+        "fraise",
+        "tomate",
+        "orange",
+        "mandarine",
+        "fraise",
+        "durian",
+        "pêche",
+        "cerise",
+        "raisin",
+        "cerise",
+    ];
+var button = document.getElementById('run');
+button.addEventListener('click', function(){
+    var newFruits = [];
+    for(var i =0; i<fruits.length;i++){
+        if(!newFruits.includes(fruits[i])){
+            newFruits.push(fruits[i]);
+        }
+    }
+    console.log(newFruits);
+})
+    // your code here
+
+})();
