dff97afd51ae51dc4f1a2ad12766e7dfd08469ff
RafSwiggers <rafswiggers@gmail.com>
2019-09-03 12:19:33 +0200

    solve 6.5

---
+++ 6 DOM/5 Hover Image/script.js
@@ -0,0 +1,25 @@
+/* becode/javascript
+ *
+ * /06-dom/05-hover-image/script.js - 6.5: survol d'image
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+    var image = document.getElementsByTagName('img')[0];
+    var originalSource = image.getAttribute('src');
+    image.addEventListener('mouseover', function(){
+        image.src = image.getAttribute('data-hover');
+    })
+    image.addEventListener('mouseout', function(){
+        image.src = originalSource;
+    })
+
+
+    // your code here
+
+})();
